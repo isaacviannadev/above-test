36a4ba2bf60e7d3585ed904f730748fc77acbed6
Isaac Vianna <60493567+isaacviannadev@users.noreply.github.com>
2025-02-20 01:10:12 +0000

    feat: add back button on details

--- src/components/episode-details/index.tsx
+++ src/components/episode-details/index.tsx
@@ -1,4 +1,5 @@
 import { EPISODES_MOCK } from '@/utils/mocks/episodes';
+import { ArrowLeft } from 'lucide-react';
 import { NavLink } from 'react-router';
 import AlertDelete from '../alert-delete';
 import { Button } from '../ui/button';
@@ -11,10 +12,16 @@ const EpisodeDetails = ({ episodeId }: EpisodeDetailsProps) => {
   const episode = EPISODES_MOCK.find((episode) => episode.id === +episodeId);
 
   return (
-    <div className='group lg:gap-8 lg:grid lg:grid-cols-3'>
+    <div className='group relative lg:gap-8 lg:grid lg:grid-cols-3'>
+      <NavLink
+        to='/'
+        className='top-0 z-20 absolute bg-transparent hover:bg-white/70 hover:shadow-md p-2 rounded-full text-zinc-600 hover:text-sky-700'
+        aria-label='Back'
+      >
+        <ArrowLeft className='size-6 hover:scale-110' />
+      </NavLink>
       <div className='relative lg:col-span-2 mb-6 lg:mb-0 rounded-lg aspect-[3/4] sm:aspect-[4/3] lg:aspect-[16/9] overflow-hidden'>
         <div className='z-10 absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-slate-900 rounded-lg' />
-
         <img
           src='https://placehold.co/600x400'
           alt={`Episode ${episode?.id}`}
